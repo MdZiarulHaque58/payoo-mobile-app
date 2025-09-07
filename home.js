7435e0fc82883853e93b239c59d8e59671d72a48
Md Ziarul Haque <mdziarulhaque555@gmail.com>
2025-09-07 20:59:25 +0600

    Implement the Cash out feature

--- home.js
+++ home.js
@@ -1,3 +1,7 @@
+// Add money feature-------------
+
+const validPin = 1234
+
 document.getElementById("add-money-btn").addEventListener("click", function(e){
     e.preventDefault()
     console.log("Hello world")
@@ -5,14 +9,49 @@ document.getElementById("add-money-btn").addEventListener("click", function(e){
     const accountNumber = document.getElementById("account-number").value
     const amount = parseInt(document.getElementById("add-amount").value)
 
-    const pin = document.getElementById("add-pin")
+    const pin = parseInt(document.getElementById("add-pin").value)
 
     const availableBalance = parseInt(document.getElementById("available-balance").innerText)
 
+    if(accountNumber.length <11){
+        alert("please provide valid account number")
+        return;
+    }
+
+    if(pin !== validPin){
+        alert("Please provide a valid pin number")
+        return;
+    }
 
     const totalNewAvailableBalance = amount+availableBalance
 
     document.getElementById("available-balance").innerText = totalNewAvailableBalance
 
 
+})
+
+// CashOut money feature
+
+document.getElementById("withdraw-btn").addEventListener("click", function(e){
+    e.preventDefault()
+    const amount = parseInt(document.getElementById("withdraw-amount").value)
+    const availableBalance = parseInt(document.getElementById("available-balance").innerText) 
+
+    const totalNewAvailableBalance = availableBalance - amount
+
+    console.log(totalNewAvailableBalance)
+
+    document.getElementById("available-balance").innerText = totalNewAvailableBalance
+})
+
+
+// toggleing feature----------
+
+document.getElementById("add-button").addEventListener("click",function(){
+    document.getElementById("cash-out-parent").style.display = "none"
+    document.getElementById("add-money-parent").style.display = "block"
+})
+document.getElementById("cash-out-button").addEventListener("click",function(){
+    document.getElementById("add-money-parent").style.display = "none"
+    document.getElementById("cash-out-parent").style.display = "block"
 })
